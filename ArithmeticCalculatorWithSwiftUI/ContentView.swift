db957c35d7fb5a48726523dc608b16a6cdadb1bd
Gaurav Tak <gaurav.tak@roro.io>
2023-08-05 23:44:42 +0530

    CalcButtons Added

--- ArithmeticCalculatorWithSwiftUI/ContentView.swift
+++ ArithmeticCalculatorWithSwiftUI/ContentView.swift
@@ -7,6 +7,39 @@
 
 import SwiftUI
 
+enum CalcButton: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case add = "+"
+    case subtract = "-"
+    case divide = "÷"
+    case mutliply = "x"
+    case equal = "="
+    case clear = "AC"
+    case decimal = "."
+    case percent = "%"
+    case negative = "+/-"
+
+    var buttonBackgroundColor: Color {
+        switch self {
+        case .add, .subtract, .mutliply, .divide, .equal:
+            return .blue
+        case .clear, .negative, .percent:
+            return Color(.lightGray)
+        default:
+            return Color(UIColor(red: 55/255.0, green: 55/255.0, blue: 55/255.0, alpha: 1))
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
