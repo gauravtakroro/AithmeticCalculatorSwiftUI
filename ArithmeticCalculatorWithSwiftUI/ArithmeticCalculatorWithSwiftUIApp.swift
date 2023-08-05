05ce8ba67de0bf882244ea90e3f683281e176f54
Gaurav Tak <gaurav.tak@roro.io>
2023-08-05 22:20:31 +0530

    Initial Commit

---
+++ ArithmeticCalculatorWithSwiftUI/ArithmeticCalculatorWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticCalculatorWithSwiftUIApp.swift
+//  ArithmeticCalculatorWithSwiftUI
+//
+//  Created by Roro Solutions LLP on 05/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticCalculatorWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
